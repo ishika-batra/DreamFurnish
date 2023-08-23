6b9e2b03e466aa695299f72769d4b08186774280
ishika2203 <ishikabatra.net@gmail.com>
2023-08-24 00:04:13 +0530

    products display added

---
+++ app.js
@@ -0,0 +1,69 @@
+// Variables
+
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
+
+// cart
+let cart = [];
+
+// getting hte products
+class Products {
+  async getProducts() {
+    try {
+      let resp = await fetch('products.json');
+      let data = await resp.json();
+
+      let products = data.items;
+      products = products.map((item) => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+// display products
+class UI {
+  displayProducts(products) {
+    let result = '';
+    products.forEach((product) => {
+      result += `
+      <!-- single product -->
+      <article class="product">
+        <div class="img-container">
+          <img src= ${product.image} alt="product" class="product-img">
+          <button class="bag-btn" data-id=${product.id}>
+            <i class="fas fa-shopping-cart"></i>
+            Add to bag
+          </button>
+        </div>
+        <h3>${product.title}</h3>
+        <h4>$${product.price}</h4>
+      </article>
+      <!-- end of single product -->`;
+    });
+    productsDOM.innerHTML = result;
+  }
+}
+
+// local storage
+class Storage {}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ui = new UI();
+  const products = new Products();
+
+  products.getProducts().then((products) => ui.displayProducts(products));
+});
